4aff7fd8b21189d3e305a30a2f29c61960d0f63a
Jacob Hummer <jcbhmr@outlook.com>
2023-06-15 12:05:28 -0500

    remove npm:test

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -1,23 +1,20 @@
-// Using 'npm:test' for Node.js v16 compat
-import test from "test";
+import test from "node:test";
 import assert from "node:assert";
 import * as index from "../index.js";
 
-test("exports are all there", async () => {
-  console.debug("index=%s", index);
+console.debug("index=%s", index);
 
-  // https://docs.rs/rfd/latest/rfd/all.html
-  const structs = [
-    "AsyncFileDialog",
-    "AsyncMessageDialog",
-    "FileDialog",
-    "FileHandle",
-    "MessageDialog",
-  ];
-  const enums = ["MessageButtons", "MessageLevel"];
-  for (const name of structs.concat(enums)) {
-    await test(`exports ${name}`, () => {
-      assert.notEqual(index[name], undefined);
-    });
-  }
-});
+// https://docs.rs/rfd/latest/rfd/all.html
+const structs = [
+  "AsyncFileDialog",
+  "AsyncMessageDialog",
+  "FileDialog",
+  "FileHandle",
+  "MessageDialog",
+];
+const enums = ["MessageButtons", "MessageLevel"];
+for (const name of structs.concat(enums)) {
+  test(`exports ${name}`, () => {
+    assert.notEqual(index[name], undefined);
+  });
+}
