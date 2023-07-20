b47baa6caaf7a67202adfa9aebdf7e491eabec22
Jacob Hummer <jcbhmr@outlook.com>
2023-07-19 20:32:03 -0500

    fixup pkg

--- tools/build-if-needed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-if ! test -f index.*.node || [[ ! -f index.js || ! -f index.d.ts ]]; then
-  if [[ $NODE_ENV == production ]]; then
-    napi build --platform --release
-  else
-    napi build --platform
-  fi
-fi
