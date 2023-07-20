88d65562a79239f957d31ab4a4aaaa94204bd974
Jacob Hummer <jcbhmr@outlook.com>
2023-07-20 02:00:34 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@
 extern crate napi_derive;
 
 mod async_file_dialog;
-pub use async_file_dialog::*;
 mod async_message_dialog;
-pub use async_message_dialog::*;
 mod file_dialog;
-pub use file_dialog::*;
 mod file_handle;
-pub use file_handle::*;
 mod message_buttons;
-pub use message_buttons::*;
 mod message_dialog;
-pub use message_dialog::*;
 mod message_level;
+
+pub use async_file_dialog::*;
+pub use async_message_dialog::*;
+pub use file_dialog::*;
+pub use file_handle::*;
+pub use message_buttons::*;
+pub use message_dialog::*;
 pub use message_level::*;
