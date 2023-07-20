e1ef97cb58a004eb540cac4856b43c60c8d8507a
Jacob Hummer <jcbhmr@outlook.com>
2023-07-19 22:27:40 -0500

    add docs

--- src/async_message_dialog.rs
+++ src/async_message_dialog.rs
@@ -3,6 +3,21 @@ use super::message_level::*;
 use napi::bindgen_prelude::*;
 use rfd;
 
+/// The builder for an asynchronous message dialog popup window. Use this to
+/// show alert boxes and other messages to the user.
+///
+/// Example:
+///
+/// ```js
+/// const answer = await new AsyncMessageDialog()
+///     .setLevel('Error')
+///     .setTitle('Uh oh!')
+///     .setDescription('Something went wrong.')
+///     .setButtons('Ok')
+///     .show();
+/// console.log(answer);
+/// //=> true
+/// ```
 #[napi]
 #[repr(transparent)]
 pub struct AsyncMessageDialog(pub(super) Option<rfd::AsyncMessageDialog>);
@@ -13,6 +28,17 @@ impl AsyncMessageDialog {
         return Self(Some(rfd::AsyncMessageDialog::new()));
     }
 
+    /// Sets the level of the message dialog. This determines the icon that is
+    /// shown in the dialog and possibly additional platform-specific defaults.
+    /// Defaults to `Info`.
+    ///
+    /// Example:
+    ///
+    /// ```js
+    /// const answer = await new AsyncMessageDialog()
+    ///     .setLevel('Error')
+    ///     .show();
+    /// ```
     #[napi]
     pub fn set_level(&mut self, level: MessageLevel) -> Result<Self> {
         if self.0.is_none() {
@@ -23,6 +49,16 @@ impl AsyncMessageDialog {
         return Ok(Self(Some(x)));
     }
 
+    /// Sets the title of the message dialog. Defaults to an empty string.
+    /// Returns `this` for chaining.
+    ///
+    /// Example:
+    ///
+    /// ```js
+    /// const answer = await new AsyncMessageDialog()
+    ///     .setTitle('Uh oh!')
+    ///     .show();
+    /// ```
     #[napi]
     pub fn set_title(&mut self, title: String) -> Result<Self> {
         if self.0.is_none() {
@@ -33,6 +69,16 @@ impl AsyncMessageDialog {
         return Ok(Self(Some(x)));
     }
 
+    /// Sets the description of the message dialog. Defaults to an empty string.
+    /// Returns `this` for chaining.
+    ///
+    /// Example:
+    ///
+    /// ```js
+    /// const answer = await new AsyncMessageDialog()
+    ///     .setDescription('Something went wrong.')
+    ///     .show();
+    /// ```
     #[napi]
     pub fn set_description(&mut self, description: String) -> Result<Self> {
         if self.0.is_none() {
@@ -43,6 +89,15 @@ impl AsyncMessageDialog {
         return Ok(Self(Some(x)));
     }
 
+    /// Sets the buttons of the message dialog. Defaults to `Ok`.
+    ///
+    /// Example:
+    ///
+    /// ```js
+    /// const answer = await new AsyncMessageDialog()
+    ///     .setButtons('Ok')
+    ///     .show();
+    /// ```
     #[napi]
     pub fn set_buttons(&mut self, buttons: MessageButtons) -> Result<Self> {
         if self.0.is_none() {
@@ -53,6 +108,22 @@ impl AsyncMessageDialog {
         return Ok(Self(Some(x)));
     }
 
+    /// Shows the message dialog and returns a promise that resolves to `true`
+    /// if the user clicked the primary button, or `false` if the user clicked
+    /// the secondary button or closed the dialog.
+    ///
+    /// Note that this will **consume the builder** so that it cannot be used
+    /// again!
+    ///
+    /// Example:
+    ///
+    /// ```js
+    /// const answer = await new AsyncMessageDialog()
+    ///     .setButtons('Ok')
+    ///     .show();
+    /// console.log(answer);
+    /// //=> true
+    /// ```
     #[napi]
     pub async unsafe fn show(&mut self) -> Result<bool> {
         if self.0.is_none() {
